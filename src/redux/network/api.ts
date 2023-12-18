10a5145cbacb6e328d3ee53165925e7029d22ead
Wael Naamah <w.naamah@tenderd.com>
2023-12-18 10:14:41 +0400

    change API_URI

--- src/redux/network/api.ts
+++ src/redux/network/api.ts
@@ -1,2 +1,2 @@
-export const API_URL: string = 'http://localhost:11700'
-//  "https://tranquil-island-76806-0b8732f212dd.herokuapp.com";
+export const API_URL: string = // 'http://localhost:11700'
+ "https://tranquil-island-76806-0b8732f212dd.herokuapp.com";
