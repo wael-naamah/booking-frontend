8ec64be063fceb241907d6903368ad595a27c284
Wael Naamah <w.naamah@tenderd.com>
2024-03-05 15:51:29 +0400

    disconnect from localhost

--- src/redux/network/api.ts
+++ src/redux/network/api.ts
@@ -1,4 +1,4 @@
-export const API_URL: string = 'http://localhost:11700'
-//  "https://tranquil-island-76806-0b8732f212dd.herokuapp.com";
+export const API_URL: string = // 'http://localhost:11700'
+ "https://tranquil-island-76806-0b8732f212dd.herokuapp.com";
 
 export const FILES_STORE: string = 'https://storage.googleapis.com/b-gas-13308.appspot.com/'
