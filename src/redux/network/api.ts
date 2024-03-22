e518c21979561190486aaada9cd6159789c64549
Wael Naamah <wael.naamah@gmail.com>
2024-03-22 10:32:27 +0400

    update favicon and backend url

--- src/redux/network/api.ts
+++ src/redux/network/api.ts
@@ -1,4 +1,5 @@
-export const API_URL: string =  'http://localhost:11700'
-//  "https://tranquil-island-76806-0b8732f212dd.herokuapp.com";
+export const API_URL: string = 'https://bgas-kalender.at/api/';
+// 'http://localhost:11700'
+// "https://tranquil-island-76806-0b8732f212dd.herokuapp.com";
 
 export const FILES_STORE: string = 'https://storage.googleapis.com/b-gas-13308.appspot.com/'
